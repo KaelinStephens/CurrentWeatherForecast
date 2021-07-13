9f0ee20c90dbb60ce00815ee12b8f972a01b7277
TheWookersquatch <ksprettygurl@gmail.com>
2021-07-13 14:28:07 -0600

    initial commit

---
+++ CurrentWeatherForecast/City.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace CurrentWeatherForecast
+{
+    public class City
+    {
+        public City()
+        {
+
+        }
+
+        public double Temp { get; set; }
+        public string Name { get; set; }
+
+        public double TempInF(double tempInK)
+        {
+            var tempInF = ((9 / 5)*(tempInK - 273)) + 32;
+            return tempInF;
+        }
+    }
+}
---
+++ CurrentWeatherForecast/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace CurrentWeatherForecast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var city = new City();
+            Console.WriteLine("The current temperature in Dubai is:");
+            Console.WriteLine();
+            Console.WriteLine("..................................");
+            Console.WriteLine();
+            var forecastResponse = client.GetStringAsync("api.openweathermap.org/data/2.5/weather?q=Dubai&appid=8b5de2303eb9da834cb65d5ed38bd581").Result;
+            
+            var forcastCurrent = JObject.Parse(forecastResponse).GetValue("temp").ToString();
+
+            city.Name = "Dubai";
+            city.Temp = city.TempInF(double.Parse(forcastCurrent));
+            Console.WriteLine($"{city.Temp} degrees F");
+        }
+    }
+}
